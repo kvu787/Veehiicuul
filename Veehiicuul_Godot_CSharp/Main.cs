f749c480663580fe1010a431bb8853e16cb1a0c4
Kevin Vu <kevin.m.vu@gmail.com>
2026-09-19 11:55:28 -0700

    Add system info printout to startup

--- Veehiicuul_Godot_CSharp/Main.cs
+++ Veehiicuul_Godot_CSharp/Main.cs
@@ -6,6 +6,7 @@ public partial class Main : Node {
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
         GD.Print("Meow");
+        DebugInfo.PrintSystemInfo_WindowsX64();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
