d62522327924e98ae4726d3c27b2dd48147c0239
Kevin Vu <kevin.m.vu@gmail.com>
2026-09-26 00:18:01 -0700

    Adjust import script to only affect StandardMaterial3D and also change more properties

--- Veehiicuul_Godot_CSharp/Veehiicuul/Source/Editor/DisableSpecularImport.gd
+++ Veehiicuul_Godot_CSharp/Veehiicuul/Source/Editor/DisableSpecularImport.gd
@@ -13,8 +13,13 @@ func _disable_specular(node: Node) -> void:
     if mesh_instance != null and mesh_instance.mesh != null:
         for surface in range(mesh_instance.mesh.get_surface_count()):
             var material := mesh_instance.get_active_material(surface)
-            if material is BaseMaterial3D:
+            if material is StandardMaterial3D:
+                material.diffuse_mode = BaseMaterial3D.DIFFUSE_LAMBERT
+                material.specular_mode = BaseMaterial3D.SPECULAR_DISABLED
+                material.disable_fog = true
+                material.disable_specular_occlusion = true
                 material.metallic_specular = 0.0
+                material.disable_receive_shadows = true
 
     for child in node.get_children():
         _disable_specular(child)
